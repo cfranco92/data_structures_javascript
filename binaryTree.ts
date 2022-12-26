924b9c9c281da6f41a3c08e6659b119aaf5ee029
Cristian Franco <cfrancobedoya@gmail.com>
2022-12-26 13:30:43 -0500

    MAIN: Add Binary tree data structure

---
+++ binaryTree.ts
@@ -0,0 +1,17 @@
+class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const root = new BinaryTree(1);
+root.left = new BinaryTree(2);
+root.right = new BinaryTree(3);
+
+console.log(root.left); // BinaryTree { value: 2, left: null, right: null }
